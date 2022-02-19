0682b89ce911ce15e8706defd5c3594c228c8d6c
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-02-19 14:59:03 +0100

    Привет мир

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const { exec } = require("child_process");
-exec("git add app.js && git commit -m 'Hola Mundo :heart: ' ", (error, stdout, stderr) => {
+exec(
+  "git add app.js && git commit -m 'Привет мир' ",
+  (error, stdout, stderr) => {
     if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+      console.log(`error: ${error.message}`);
+      return;
     }
     if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+      console.log(`stderr: ${stderr}`);
+      return;
     }
     console.log(`stdout: ${stdout}`);
-});
+  }
+);
