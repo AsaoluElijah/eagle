284ff4b5e0f8a6e7f16f30bb915f4a29ae93d0f9
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-03-19 15:26:10 +0100

    ..

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const { exec } = require("child_process");
-exec(
-  "git add app.js && git commit -m 'Привет мир' ",
-  (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
+const fs = require("fs");
+
+let commitCount = 0;
+
+const startMagic = () => {
+  let fileContent = fs.readFileSync("a.txt");
+
+  if (fileContent == "Привет") {
+    fs.writeFileSync("a.txt", "до свидания");
+  } else {
+    fs.writeFileSync("a.txt", "Привет");
   }
-);
+
+  exec(
+    "git add a.txt && git commit -m 'Привет мир' ",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      // console.log(`stdout: ${stdout}`);
+      commitCount++;
+    }
+  );
+  console.log(commitCount);
+};
+
+setInterval(startMagic, 1000);
